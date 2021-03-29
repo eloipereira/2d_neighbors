78c44da9023fb2e6741c8841f8ef894c1fc84e1b
Eloi Pereira <eloi@berkeley.edu>
2021-03-30 00:26:43 +0100

    add .gitignore

--- 2d_neighbors.py
+++ 2d_neighbors.py
@@ -6,7 +6,6 @@ class Point(object):
     def __init__(self,x,y):
         self.x = x
         self.y = y
-        self.dist2origin = x + y
     def __str__(self):
         return f'{self.x}, {self.y}'
     def __repr__(self):
